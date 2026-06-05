6129bea958d861fe7b7b1ff011fba937245b37a3
andreialexandrustoica09-dot <andrei.alexandru.stoica09@gmail.com>
2026-06-05 20:48:05 +0300

    Update service worker v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='athlete-hub-v1';
-const ASSETS=['./','/Athlete-hub/index.html','/Athlete-hub/manifest.json'];
+const CACHE='ah-v2';
+const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return res}).catch(()=>cached)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return res}).catch(()=>r)))});
